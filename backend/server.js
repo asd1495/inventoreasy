b5467bca16da0f4791a853d22567f43175e6d68c
asd1495 <armando.salcedo95@gmail.com>
2023-12-06 18:09:05 -0600

    added userModel schema

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
 
 //Connect to db and start server
+const PORT = process.env.PORT || 5000;
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
